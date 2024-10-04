2e7d430e87840ecb4ab66fb6dea13ccf32c328d5
Alok-Codecamp <alok61.bd@gmail.com>
2024-10-05 01:12:42 +0600

    background change

--- src/App.jsx
+++ src/App.jsx
@@ -48,14 +48,14 @@ function App() {
   };
 
   return (
-   <>
+   <div className='bg-slate-900'>
      <Home/>
      <Skills/>
      <RecentWork/>
      <Contact/>
      <About/>
      <Blog/>
-     </>
+     </div>
   );
 }
 
